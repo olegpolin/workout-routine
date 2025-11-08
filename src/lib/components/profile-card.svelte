3bf49d3886771ab1290cd75db269b4a8d7507a2c
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2025-11-08 14:11:22 -0500

    Add profile card component

---
+++ src/lib/components/profile-card.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import * as Item from '$lib/components/ui/item';
+  import * as Avatar from '$lib/components/ui/avatar';
+  import UserIcon from '@lucide/svelte/icons/user';
+
+  interface Props {
+    class?: string;
+    profile: {
+			username: string;
+			full_name: string | null;
+			avatar_url: string | null;
+		} | null;
+  }
+
+  let { class: className, profile }: Props = $props();
+</script>
+
+<Item.Root class={className}>
+  <Item.Media>
+    <Avatar.Root>
+      <Avatar.Image src={profile?.avatar_url} alt="Profile picture" />
+      <Avatar.Fallback>
+        <UserIcon />
+      </Avatar.Fallback>
+    </Avatar.Root>
+  </Item.Media>
+  <Item.Content class="gap-1">
+    <Item.Title>{profile?.full_name ?? 'Unknown User'}</Item.Title>
+    <Item.Description>@{profile?.username ?? 'unknown-user'}</Item.Description>
+  </Item.Content>
+</Item.Root>
