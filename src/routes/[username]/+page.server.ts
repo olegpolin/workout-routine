11ed72ffce275176154091471f3a7ae716af1946
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2025-11-21 13:03:51 -0500

    Update profile route

---
+++ src/routes/[username]/+page.server.ts
@@ -0,0 +1,18 @@
+import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
+
+export const load: PageServerLoad = async ({ params, locals: { supabase } }) => {
+  const { data: userProfile } = await supabase
+    .from('profiles')
+    .select('username, full_name, avatar_url')
+    .eq('username', params.username)
+    .single();
+
+  if (!userProfile) {
+    error(404, 'Not Found');
+  }
+
+  return {
+    userProfile
+  };
+};
