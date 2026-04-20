d9ce2e1a344c7af9c75e17f4f6cd461ef10df0e7
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-04-20 16:05:08 -0400

    Add Supabase types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,4 +1,5 @@
 import type { SupabaseClient, Session, User } from '@supabase/supabase-js';
+import type { Database } from '$lib/database.types';
 
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
@@ -6,7 +7,7 @@ declare global {
   namespace App {
     // interface Error {}
     interface Locals {
-      supabase: SupabaseClient;
+      supabase: SupabaseClient<Database>;
       safeGetSession: () => Promise<{ session: Session | null; user: User | null }>;
       session: Session | null;
       user: User | null;
