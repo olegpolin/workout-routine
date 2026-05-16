b92326c46b88da037270740ebad82de8baf02063
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-05-16 12:53:54 -0400

    Update sonner style

--- src/lib/components/ui/sonner/sonner.svelte
+++ src/lib/components/ui/sonner/sonner.svelte
@@ -14,6 +14,9 @@
 	theme={mode.current}
 	class="toaster group"
 	style="--normal-bg: var(--color-popover); --normal-text: var(--color-popover-foreground); --normal-border: var(--color-border);"
+	toastOptions={{
+		class: 'border! border-border! shadow-md!'
+	}}
 	{...restProps}
 >
 	{#snippet loadingIcon()}
